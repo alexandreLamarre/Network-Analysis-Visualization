1d24c6a79d1256b8418dae2341d8f8de1382f72c
alexandreLamarre <alex7285@gmail.com>
2020-10-15 20:41:26 -0400

    Almost fully functional custom networks

--- src/NetworkCustom.js
+++ src/NetworkCustom.js
@@ -4,25 +4,35 @@ import Vertex from "./datatypes/Vertex";
 
 import "./NetworkCustom.css";
 
+const VERTEX_SIZE = 5;
+const VERTEX_COLOR = "rgb(0,255,255)";
+const SELECTED_COLOR = "rgb(0,255,0)";
+
 class NetworkCustomVisualizer extends React.Component{
   constructor(props){
     super(props);
     this.state = {
       height: 0,
       width: 0,
-      gridConstant: 20,
+      gridConstant: 10,
+      gridX: 0,
+      gridY: 0,
       mouseX: 0,
       mouseY: 0,
       previousMouseX: 0,
       previousMouseY: 0,
       dragging: false,
       edgeStart: [null,null],
+      edgeStartIndex: 0,
       mouseLeave: true,
+      vertex_list: [],
+      edge_list: [],
       vertices: {},
       edges: {},
       operationType: "newVertex",
       opererationsBuffer: [],
       operationsBufferIndex: 0,
+      selected_vertices: null,
       box: null,
     }
     this.app = this.props.app;
@@ -38,13 +48,13 @@ class NetworkCustomVisualizer extends React.Component{
     const gridY = h/c;
     const vertices = {}
     for(let i = 0; i < c; i++){
-      const x = Math.round((gridX*i+ Number.EPSILON) * 100) / 100;
+      // const x = Math.floor(Math.round((gridX*i+ Number.EPSILON) * 100) / 100);
       for(let j = 0; j < c; j++){
-        const y = Math.round((gridY*j+ Number.EPSILON) * 100) / 100;
-        vertices[[x,y]] = [];
+        // const y = Math.floor(Math.round((gridY*j+ Number.EPSILON) * 100) / 100);
+        vertices[[i,j]] = [];
       }
     }
-    this.setState({height: h, width: w, vertices: vertices});
+    this.setState({height: h, width: w, vertices: vertices, gridX: gridX, gridY: gridY});
   }
 
   componentDidUpdate(){
@@ -54,28 +64,42 @@ class NetworkCustomVisualizer extends React.Component{
     const ctx = this.canvas.current.getContext("2d");
     ctx.scale(this.state.scale, this.state.scale);
     ctx.clearRect(0,0,this.state.width, this.state.height);
-    for(var key in this.state.vertices){
-      if(this.state.vertices[key].length !== 0){
-        for(let i = 0; i < this.state.vertices[key].length; i++){
-          const v = this.state.vertices[key][i];
-          ctx.beginPath();
-          ctx.fillStyle = v.color;
-          ctx.arc(v.x, v.y,v.size, 0, Math.PI*2);
-          ctx.fill();
-          ctx.closePath();
-        }
-      }
+    for(let i = 0; i < this.state.vertex_list.length; i++){
+      const v = this.state.vertex_list[i];
+      ctx.beginPath();
+      ctx.fillStyle = v.color;
+      ctx.arc(v.x,v.y, v.size, 0, Math.PI*2);
+      ctx.fill();
+      ctx.closePath();
     }
 
-    for(var key in this.state.edges){
+
+    //draw grid
+    for(let i = 1; i < this.state.gridConstant; i++){
+      ctx.beginPath();
+      ctx.moveTo(i*this.state.gridX, 0);
+      ctx.lineTo(i*this.state.gridX, this.state.height);
+      ctx.stroke();
+      ctx.closePath();
+    }
+    for(let i = 1; i < this.state.gridConstant; i++){
+      ctx.beginPath();
+      ctx.moveTo(0, this.state.gridY*i);
+      ctx.lineTo(this.state.width, this.state.gridY*i);
+      ctx.stroke();
+      ctx.closePath();
+    }
+
+    for(let i = 0; i < this.state.edge_list.length; i++){
       // console.log("edge", key);
-      const values = key.split(",");
-      const [startX, startY, endX, endY] = values;
+      const e = this.state.edge_list[i];
+      const v1 = this.state.vertex_list[e[0]];
+      const v2 = this.state.vertex_list[e[1]];
       // console.log(startX, startY, endX, endY)
       ctx.beginPath();
       ctx.fillStyle = "rgb(0,0,0)";
-      ctx.moveTo(startX, startY);
-      ctx.lineTo(endX, endY);
+      ctx.moveTo(v1.x, v1.y);
+      ctx.lineTo(v2.x, v2.y);
       ctx.globalAlpha = 0.3;
       ctx.stroke();
       ctx.closePath();
@@ -86,7 +110,7 @@ class NetworkCustomVisualizer extends React.Component{
       ctx.fillStyle = rgb_to_str(this.app.state.startRed,this.app.state.startGreen,
                                     this.app.state.startBlue);
       ctx.globalAlpha= 0.7;
-      ctx.arc(this.state.mouseX, this.state.mouseY, this.app.state.minsize, 0, 2*Math.PI);
+      ctx.arc(this.state.mouseX, this.state.mouseY, VERTEX_SIZE, 0, 2*Math.PI);
       ctx.fill();
       ctx.closePath();
     }
@@ -103,6 +127,7 @@ class NetworkCustomVisualizer extends React.Component{
     }
     if(this.state.box !== null){
       ctx.beginPath();
+      ctx.lineWidth = 2;
       ctx.setLineDash([5, 15]);
       ctx.moveTo(this.state.box[0], this.state.box[1]);
       ctx.lineTo(this.state.box[2], this.state.box[1]);
@@ -125,13 +150,14 @@ class NetworkCustomVisualizer extends React.Component{
     const prevY = this.state.previousY;
     const vertices = this.state.vertices;
     const edge = this.state.edges;
+    const edgeStartIndex = this.state.edgeStartIndex;
     const startX = this.state.edgeStart[0];
     const startY = this.state.edgeStart[1];
     const rect = this.canvas.current.getBoundingClientRect(x,y);
     const box = this.state.box;
     // console.log(this.state.edgeStart);
     if(this.state.operationType === "ConnectEdge" && this.state.dragging === true) {
-      this.tryConnectEdge(x,y,vertices,startX, startY);
+      this.tryConnectEdge(startX, startY, edgeStartIndex, x, y, vertices);
     }
     if(this.state.operationType === "SelectMove" && this.state.dragging === true){
       this.createBox(x,y, startX, startY, box, prevX, prevY);
@@ -145,8 +171,13 @@ class NetworkCustomVisualizer extends React.Component{
   }
 
   setOperationType(v){
-    console.log(v);
-    this.setState({operationType: v, box:null})
+    console.log(this.state.vertices);
+    if(this.state.selected_vertices !== null){
+      for(let i = 0; i < this.state.selected_vertices.length; i++){
+        this.state.selected_vertices[i].vertex.color = VERTEX_COLOR;
+      }
+    }
+    this.setState({operationType: v, box:null, selected_vertices: null, edgeStart: [null, null]})
   }
 
   processClickOutcome(e){
@@ -161,67 +192,101 @@ class NetworkCustomVisualizer extends React.Component{
   placeVertex(e){
     const rect = this.canvas.current.getBoundingClientRect();
     const vertices = this.state.vertices;
+    const vertex_list = this.state.vertex_list;
     const [vertexX, vertexY] = this.getGrid(e.clientX -rect.left, e.clientY - rect.top)
     // console.log([vertexX, vertexY]);
     /* TODO CHECK if another vertex is too closed*/
-    vertices[[vertexX, vertexY]].push(new Vertex(e.clientX-rect.left, e.clientY - rect.top));
-    this.setState({vertices:vertices});
+    const v = new Vertex(e.clientX-rect.left, e.clientY - rect.top);
+    v.size = VERTEX_SIZE;
+    v.color = VERTEX_COLOR;
+    vertex_list.push(v)
+    // console.log(vertex_list.length-1);
+    vertices[[vertexX, vertexY]].push(vertex_list.length-1);
+
+    this.setState({vertices:vertices, vertex_list: vertex_list});
   }
 
-  tryConnectEdge(x,y,vertices, edge_startX, edge_startY){
+  tryConnectEdge(edge_startX, edge_startY, edgeStartIndex, x, y, vertices){
     // console.log("connecting");
     // console.log("input", x, y, vertices, edge_start, edge_end);
     if(edge_startX === null && edge_startY === null){
       const [new_x,new_y] = this.getGrid(x,y);
       // console.log(new_x, new_y);
       for(let i = 0; i < vertices[[new_x,new_y]].length; i++){
-        const v = vertices[[new_x,new_y]][i];
-        if(distance(v.x,v.y,x, y) < (this.app.state.minsize+2)*(this.app.state.minsize+2)){
-          this.setState({edgeStart: [v.x,v.y]})
+        const v = this.state.vertex_list[vertices[[new_x,new_y]][i]];
+        if(distance(v.x,v.y,x, y) < (VERTEX_SIZE+2)*(VERTEX_SIZE+2)){
+          this.setState({edgeStart: [v.x,v.y], edgeStartIndex: vertices[[new_x,new_y]][i]});
           break;
         }
       }
     }
     else{
       const [new_x,new_y] = this.getGrid(x,y);
+      console.log(vertices[[new_x, new_y]]);
       // console.log(new_x, new_y);
       for(let i = 0; i < vertices[[new_x,new_y]].length; i++){
-        const v = vertices[[new_x,new_y]][i];
-        if(distance(v.x,v.y,x, y) < (this.app.state.minsize+2)*(this.app.state.minsize+2)){
+        const v = this.state.vertex_list[vertices[[new_x,new_y]][i]];
+        // console.log(v);
+        if(distance(v.x,v.y,x, y) < (VERTEX_SIZE+2)*(VERTEX_SIZE+2)){
+          // console.log("VERTEX SPOTTED!");
           const edges = this.state.edges;
-          const new_edges = this.addEdge(edges, [edge_startX, edge_startY], [v.x, v.y])
-          this.setState({edgeStart: [v.x,v.y], edges: new_edges});
+          const edge_list = this.state.edge_list;
+          const [new_edges, new_edge_list] = this.addEdge(edges,
+                    edge_list, this.state.edgeStartIndex, vertices[[new_x,new_y]][i]);
+          this.setState({edgeStart: [v.x,v.y],
+            edgeStartIndex: vertices[[new_x, new_y]][i],edges: new_edges,
+                                                  edge_list: new_edge_list});
           break;
         }
       }
     }
   }
 
-  addEdge(edges, startVertex, endVertex){
-    // console.log("adding edge to ", edges);
-    // console.log("connecting ", startVertex, endVertex);
-    if(edges[[startVertex,endVertex]] === undefined && edges[[endVertex,startVertex]] === undefined &&
-      startVertex[0] !== endVertex[0] && startVertex[1] !== endVertex[1]){
-      console.log("successful connection")
-      const [x1,y1] = this.getGrid(startVertex[0], startVertex[1]);
-      const [x2,y2] = this.getGrid(endVertex[0], endVertex[1]);
-      edges[[startVertex, endVertex]] = {startGrid: [x1,y1], endGrid: [x2,y2]};
-      console.log(edges);
+  addEdge(edges, edge_list, startVertexIndex, endVertexIndex){
+    // // console.log("adding edge to ", edges);
+    // // console.log("connecting ", startVertex, endVertex);
+    // console.log(edges[[startVertexIndex, endVertexIndex]]);
+    // console.log(edges[[endVertexIndex, startVertexIndex]]);
+    if(edges[[startVertexIndex,endVertexIndex]] === undefined &&
+      edges[[endVertexIndex, startVertexIndex]] === undefined &&
+                    startVertexIndex !== endVertexIndex){
+      // console.log("successful connection");
+      edges[[startVertexIndex, endVertexIndex]] = true;
+      edge_list.push([startVertexIndex, endVertexIndex]);
     }
-    return edges;
+    return [edges, edge_list];
+    // if(edges[[startVertex,endVertex]] === undefined && edges[[endVertex,startVertex]] === undefined &&
+    //   startVertex[0] !== endVertex[0] && startVertex[1] !== endVertex[1]){
+    //   console.log("successful connection")
+    //   const [x1,y1] = this.getGrid(startVertex[0], startVertex[1]);
+    //   const [x2,y2] = this.getGrid(endVertex[0], endVertex[1]);
+    //   edges[[startVertex, endVertex]] = {startGrid: [x1,y1], endGrid: [x2,y2]};
+    //   console.log(edges);
+    // }
+    // return edges;
   }
 
   createBox(x,y, startX,startY, box, previousX, previousY){
     if(startX === null && startY === null){
       if(box === null){
-      this.setState({edgeStart: [x,y]});
+        const selected_vertices = this.state.selected_vertices;
+        if(selected_vertices !== null){
+          for(let i = 0; i < selected_vertices.length; i++){
+            selected_vertices[i].vertex.color = VERTEX_COLOR;
+          }
+        }
+      this.setState({edgeStart: [x,y], selected_vertices: null});
       }
       else{
         if(Math.min(box[0], box[2]) < x && x < Math.max(box[0], box[2]) &&
           Math.min(box[1], box[3])< y && y<Math.max(box[1], box[3])){
-            const deltaX = x - previousX;
-            const deltaY = y - previousY;
-            // this.updateVertices(box[0], box[1], box[2], box[3], deltaX, deltaY);
+            var deltaX = x - previousX;
+            var deltaY = y - previousY;
+            const selected_vertices = this.state.selected_vertices;
+            if(box[0]+deltaX<0 || box[2]+deltaX > this.state.width) deltaX = 0;
+            if(box[1]+deltaY<0 || box[3]+deltaY > this.state.height) deltaY = 0;
+            const vertices = this.state.vertices;
+            this.updateSelectedVertices(vertices, selected_vertices, deltaX, deltaY);
             this.setState({box: [box[0]+deltaX, box[1]+deltaY, box[2]+deltaX, box[3]+deltaY]});
           }
           else{
@@ -231,57 +296,134 @@ class NetworkCustomVisualizer extends React.Component{
     }
     else{
       // console.log(startX, startY, x, y);
-      this.setState({box: [startX, startY, x, y]});
+      this.setState({box: [Math.min(startX,x), Math.min(startY,y), Math.max(x,startX), Math.max(y,startY)]});
     }
   }
 
-  updateVertices(x0,y0,x1,y1, deltaX, deltaY){
-    const gridToUpdate = this.getBiggerGrid(x0,y0,x1,y1);
-    const vertices = this.state.vertices;
-    for(let i = 0; i < gridToUpdate.length; i++){
-      for(let j = 0; j < gridToUpdate[i].length; j++){
-        vertices[gridToUpdate[i]][j].x += deltaX;
-        vertices[gridToUpdate[i]][j].x += deltaY;
-      }
+  updateSelectedVertices(vertices,selected_vertices, dx, dy){
+    var new_vertices = vertices;
+    for(let i = 0; i < selected_vertices.length; i++){
+      selected_vertices[i].vertex.x += dx;
+      selected_vertices[i].vertex.y += dy;
+      const new_grid = [selected_vertices[i].grid[0], selected_vertices[i].grid[1]];
+      // console.log("vertex x", selected_vertices[i].vertex.x)
+      // console.log("gridX", selected_vertices[i]);
+      if(selected_vertices[i].vertex.x > selected_vertices[i].grid[0]*this.state.gridX +this.state.gridX) new_grid[0]++;
+      if(selected_vertices[i].vertex.x < selected_vertices[i].grid[0]*this.state.gridX) new_grid[0]--;
+      if(selected_vertices[i].vertex.y > selected_vertices[i].grid[1]*this.state.gridY + this.state.gridY) new_grid[1]++;
+      if(selected_vertices[i].vertex.y < selected_vertices[i].grid[1]*this.state.gridY) new_grid[1]--;
+      if(new_grid[0] !== selected_vertices[i].grid[0] || new_grid[1]
+        !== selected_vertices[i].grid[1]){
+          new_vertices = this.updateVertexGrids(vertices,selected_vertices[i], new_grid);
+        };
     }
+    this.setState({vertices: new_vertices});
   }
 
+  updateVertexGrids(vertices,selected_vertex, new_grid){
+    const old_grid = selected_vertex.grid;
+    // console.log("updating vertex grids from", old_grid, "to", new_grid);
+    const index_value = vertices[old_grid].indexOf(selected_vertex.index);
+    // console.log("element to remove", selected_vertex.index);
+    vertices[old_grid].splice(index_value,1);
+    // console.log("vertices[old+grid]",vertices[old_grid]);
+    vertices[new_grid].push(selected_vertex.index);
+    // console.log("vertices[new_grid]",vertices[new_grid]);
+    selected_vertex.grid = [new_grid];
+
+    return vertices;
+  }
 
   clearNetwork(){
-    const gridX = Math.round((this.state.width/this.state.gridConstant+Number.EPSILON)*100)/100;
-    const gridY = Math.round((this.state.height/this.state.gridConstant+Number.EPSILON)*100)/100;
     const vertices = {};
     const c = this.state.gridConstant;
     for(let i = 0; i < c; i++){
       for(let j = 0; j < c; j++){
-        vertices[[gridX*i, gridY*j]] = [];
+        vertices[[i,j]] = [];
       }
     }
-    this.setState({vertices: vertices, edges: {}, edgeStart: [null,null], box:null});
+    this.setState({vertices: vertices, edges: {}, edgeStart: [null,null], box:null,
+    selected_vertices: null, vertex_list: [], edge_list: []});
   }
 
   clearDragOutcome(){
-    this.setState({dragging:false, edgeStart: [null,null]});
+    this.setState({dragging:false, edgeStart: [null,null], edgeStartIndex: null});
+    const box = this.state.box;
+    const selected_vertices = this.state.selected_vertices;
+    if(box !== null &&  selected_vertices === null){
+      this.selectVertices(box);
+    }
   }
 
   getGrid(x,y){
-    const gridX = Math.round((this.state.width/this.state.gridConstant+Number.EPSILON)*100)/100;
-    const gridY = Math.round((this.state.height/this.state.gridConstant+Number.EPSILON)*100)/100;
+    const gridX = this.state.gridX;
+    const gridY = this.state.gridY;
 
     const new_x = Math.floor(x/gridX);
     const new_y = Math.floor(y/gridY);
-    return [new_x*gridX, new_y*gridY];
+    return [new_x, new_y];
   }
 
-  getBiggerGrid(x0,y0,x1,y1){
-    const gridX = Math.round((this.state.width/this.state.gridConstant+Number.EPSILON)*100)/100;
-    const gridY = Math.round((this.state.height/this.state.gridConstant+Number.EPSILON)*100)/100;
-    const startGrid = this.getGrid(x0, y0);
+  getListGrid(x0, y0, x1, y1){
+    const startGrid = this.getGrid(x0,y0);
     const endGrid = this.getGrid(x1,y1);
+    // console.log("startGrid", startGrid);
+    // console.log("endGrid", endGrid);
+    if(startGrid[0] === endGrid[0] && startGrid[1] === endGrid[1]) return [startGrid];
+    else if(startGrid[0] === endGrid[0]){
+      const res = [];
+      for(let i = startGrid[1]; i <= endGrid[1]; i++){
+        res.push([startGrid[0], i])
+      }
+      return res;
+    }
+    else if(startGrid[1] === endGrid[1]){
+      const res = [];
+      for(let j = startGrid[0]; j <= endGrid[0]; j++){
+        res.push([j, startGrid[1]]);
+      }
+      return res;
+    }
+    else{
+      const res = [];
+      for(let i = startGrid[1]; i <= endGrid[1]; i++){
+        for(let j = startGrid[0]; j <= endGrid[0]; j++){
+          res.push([j, i]);
+        }
+      }
+      return res;
+    }
+  }
 
-    const bigger_grid = [];
+  selectVertices(box){
+    var x0 = Math.min(box[0], box[2]);
+    var y0 = Math.min(box[1], box[3]);
+    var x1 = Math.max(box[0], box[2]);
+    var y1 = Math.max(box[1], box[3]);
 
-    return bigger_grid;
+    // console.log("coordinates", x0,y0,x1,y1);
+    const boxesToCheck = this.getListGrid(x0,y0,x1,y1);
+    // console.log("boxes", boxesToCheck);
+    const selected_vertices = this.unPackVertexFromGrid(boxesToCheck, x0, y0, x1,y1);
+    console.log("selected_vertices", selected_vertices);
+    for(let i = 0; i < selected_vertices.length; i++){
+      selected_vertices[i].vertex.color = SELECTED_COLOR;
+    }
+    this.setState({selected_vertices: selected_vertices});
+  }
+
+  unPackVertexFromGrid(box_array, x0, y0, x1, y1){
+    const selected_vertices = [];
+    // console.log(this.state.vertices);
+    for(let i = 0; i < box_array.length; i++){
+      var index_list = this.state.vertices[box_array[i]];
+      for(let j = 0; j < index_list.length; j++){
+        const v = this.state.vertex_list[index_list[j]]
+        if(v.x >= x0 && v.y >= y0 && v.x<= x1 && v.y <= y1)selected_vertices.push({vertex: v,
+                                      grid: box_array[i], index: index_list[j]});
+      }
+    }
+    return selected_vertices;
   }
 
   /*
@@ -396,6 +538,7 @@ function distance(x1,x2,y1,y2){
 function rgb_to_str(color){
   return "rgb("+color[0]+"," + color[1]+","+color[2]+")";
 }
+
 
 
 export default NetworkCustomVisualizer;
