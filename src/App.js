5b106716e9a247367fd569a89dc62623ae4856e2
alexandreLamarre <alex7285@gmail.com>
2021-03-04 18:47:56 -0500

    Main app update with abstraction mechanisms

--- src/App.js
+++ src/App.js
@@ -3,22 +3,18 @@ import React from 'react';
 import './App.css';
 
 //CUSTOM COMPONENT IMPORTS
-import {NetworkVisualizer, NetworkVisualizer3D, NetworkCustom} from "./Network/index.js"
-import {Nav2D, Nav3D, NavCustom} from "./NavBar"
-// import NetworkVisualizer from "./Network";
-import NetworkNavBar from "./Settings/NetworkNavBar";
-// import NetworkVisualizer3D from "./Network3D";
-import NetworkCustomVisualizer from "./NetworkCustom";
-
+import {NetworkVisualizer, NetworkVisualizer3D, NetworkCustom} from "./Components/Network/index.js"
+import {Nav2D, Nav3D, NavCustom} from "./Components/NavBar"
+import Settings from "./Animations/Algorithms/Settings.js"
+import SettingObject from "./Animations/Algorithms/Setting.js"
 
 
 //REACT ROUTER IMPORTS
-
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 // IONIC IMPORTS
-import {IonApp, IonGrid, IonRow, IonCol, IonLabel, IonContent, IonItem, IonIcon, IonInput} from "@ionic/react"
-
+import {IonApp, IonGrid, IonRow, IonCol,
+  IonLabel, IonContent, IonItem, IonIcon, IonInput, IonButton} from "@ionic/react"
 import '@ionic/react/css/core.css';
 /* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
@@ -39,6 +35,7 @@ class App extends React.Component{
   constructor(props){
     super(props)
     this.state = {
+      settingsHTML: null,
       running:false,
       animationSpeed: 50,
       numV: 120,
@@ -48,82 +45,30 @@ class App extends React.Component{
       minE: 20,
       height: 0,
       width: 0,
-      settings: {
-        spring: {ka: 2, kr: 1, eps: 0.5,
-                delta: 0.1, areascaling: 0,
-                distanceType: 1},
-        fruchterman: {cTemp: 1,
-                tempHeuristic: "Logarithmic", eps: 0.5},
-        forceatlas2: {fr: 10, gravity: false,
-                      gravityType: "Normal", kg: 10,
-                      tau: 0.1, ksmax: 10,
-                      overlappingNodes: true},
-        forceatlaslinlog: {fr: 10, gravity: false,
-                      gravityType: "Normal", kg: 10,
-                      tau: 0.1, ksmax: 10,
-                      overlappingNodes: true},
-        kruskal: {red: 255,
-                  green: 0,
-                  blue: 0},
-        prim: {red:255,
-               green: 0,
-               blue: 0},
-        opt2:{
-          timeout: 30,
-          red:255,
-          green: 0,
-          blue: 0,
-        },
-        opt2annealing:{
-          timeout: 30,
-          temperature: 100,
-          acceptance: 0.0001,
-          startColor: [255,0,0],
-          endColor: [0,0,255],
-        },
-        opt3:{
-          timeout: 30,
-          red:255,
-          green: 0,
-          blue: 0,
-        },
-        greedy:{
-          startRed: 0,
-          startGreen: 255,
-          startBlue: 0,
-          endRed: 255,
-          endGreen: 0,
-          endBlue: 0,
-        }
-      },
-
-
-      dimension: 2,
-      degreesize: false,
-      minsize: 3,
-      maxsize: 10,
-      startRed: 0,
-      startGreen: 255,
-      startBlue: 255,
-      endRed: 0,
-      endGreen: 255,
-      endBlue: 255,
-      filter: "",
-      filtering: false,
+      settingsObject : [],
     }
-    this.network = React.createRef();
-    this.network3d = React.createRef();
-    this.customnetwork = React.createRef();
-    this.navbar = React.createRef();
-    this.toolbar = React.createRef();
     
   }
 
   componentDidMount(){
+    let s = new Settings("Network ")
+    let s1 = SettingObject.newRangeSetting("vertices", 4, 200, 1, 4)
+    let s2 = SettingObject.newRangeSetting("edges", 0, 600, 1, 4)
+    let s3 = SettingObject.newCheckBoxSetting("force connectedness", false)
+    let s4 = SettingObject.newOptionSetting("random generation", ["random", "random-circle"], "random")
+    s.push(s1)
+    s.push(s2)
+    s.push(s3)
+    s.push(s4)
+
     const w = window.innerWidth;
     const h = window.innerHeight;
     window.addEventListener("resize", () => {this.resize()})
-    this.setState({height: h, width: w});
+    this.setState({height: h, width: w, settingsHTML: s.toHTML(), settingsObject: s});
+  }
+
+  checkImplementation(){
+    console.log(this.state.settingsObject)
   }
 
   resize(){
@@ -133,20 +78,6 @@ class App extends React.Component{
   }
 
   render() {
-
-    // let network;
-    // if(this.state.dimension === 2){
-    //   network = <NetworkVisualizer height = {this.state.height}
-    //   ref = {this.network} app = {this}/>
-    // }
-    // else if(this.state.dimension === 3){
-    //   network = <NetworkVisualizer3D
-    //   height = {this.state.height}
-    //   ref = {this.network3d} app = {this}/>
-    // }
-    // else if(this.state.dimension === "Custom"){
-    //   network = <NetworkCustomVisualizer height = {this.state.height}
-    //   ref = {this.customnetwork} app = {this}></NetworkCustomVisualizer>
     var numnetwork = 9
     var numsettings = 3
 
@@ -189,20 +120,20 @@ class App extends React.Component{
               <IonRow size = "11" style = {{overflow: "auto"}} id = "appcontent">
                 <IonCol size = {numnetwork}>
 
-                  <Route path = "/Network-Analysis-Visualization/2d" render = {() => <NetworkVisualizer offsetRef = {this.toolbar}/>}/>
-                  <Route path = "/Network-Analysis-Visualization/3d" render = {() => <NetworkVisualizer3D offsetRef = {this.toolbar}/>}/>
-                  <Route path = "/Network-Analysis-Visualization/custom" render = {() => <NetworkCustom offsetRef = {this.toolbar}/>}/>
+                  <Route path = "/Network-Analysis-Visualization/2d" render = {() => <NetworkVisualizer />}/>
+                  <Route path = "/Network-Analysis-Visualization/3d" render = {() => <NetworkVisualizer3D />}/>
+                  <Route path = "/Network-Analysis-Visualization/custom" render = {() => <NetworkCustom />}/>
 
                 </IonCol>
                 <IonCol size = {numsettings}  id = "settings">
                   <IonContent style = {{boxShadow: "5px 10px 35px grey"}}>
                   <IonItem>
                     <IonIcon icon = {searchCircleOutline}/>
-                    <IonInput placeholder = "filter">
+                    <IonInput placeholder = "filter" style = {{textAlign: "center"}}>
                     </IonInput>
                   </IonItem>
-
-
+                    {this.state.settingsHTML}
+                    <IonButton onClick = {() => this.checkImplementation()}> Log </IonButton>
                   </IonContent>
 
                 </IonCol>
@@ -213,16 +144,6 @@ class App extends React.Component{
           </Router>
         </IonApp>
     )
-    // return (
-    //   <div className="App">
-    //     <div className = "AppElements">
-    //       <NetworkNavBar height = {this.state.height}
-    //       ref = {this.navbar} app = {this}/>
-    //       {network}
-    //     </div>
-    //   </div>
-    //
-    // );
   }
 }
 export default App;
