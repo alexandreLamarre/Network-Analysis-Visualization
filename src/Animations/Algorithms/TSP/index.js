3f21050cacac7216001dc79a0ea6bd4808b0520e
alexandreLamarre <alex7285@gmail.com>
2021-03-11 14:43:24 -0500

    Added 3-opt simulated annealing

--- src/Animations/Algorithms/TSP/index.js
+++ src/Animations/Algorithms/TSP/index.js
@@ -1,5 +1,6 @@
 import Opt2 from "./opt2";
 import Opt3 from "./opt3";
 import Opt2Annealing from "./optAnnealing"
+import Opt3Annealing from "./opt3Annealing";
 
-export {Opt2, Opt3, Opt2Annealing};
+export {Opt2, Opt3, Opt2Annealing, Opt3Annealing};
--- src/Animations/Algorithms/TSP/opt3Annealing.js
+++ src/Animations/Algorithms/TSP/opt3Annealing.js
@@ -1,16 +1,18 @@
 import AbstractTSPAlgorithm from "./AbstractTSPAlgorithm";
 import AlgorithmSettingObject from "../AlgorithmSetting";
+import LinearColorGradient from "../../../datatypes/ColorGradient/LinearColorGradient";
+import PolarColorGradient from "../../../datatypes/ColorGradient/PolarColorGradient";
 
-class Opt3 extends AbstractTSPAlgorithm{
+class Opt3Annealing extends AbstractTSPAlgorithm{
     constructor(){
-        super("3-Opt")
+        super("3-Opt Simulated Annealing")
 
         this.iterations = AlgorithmSettingObject.newRangeSetting(
             "Maximum Possible Swaps",
             100,
             10000,
             1,
-            5000);
+            5025);
 
         this.simulations = AlgorithmSettingObject.newRangeSetting(
             "Attempts Per Iteration",
@@ -20,13 +22,56 @@ class Opt3 extends AbstractTSPAlgorithm{
             100
         )
 
-        this.selectedColor = AlgorithmSettingObject.newColorSetting(
-            "Color",
-            "#ff0000"
+        this.initialTemperature = AlgorithmSettingObject.newRangeSetting(
+            "Initial System Temperature",
+            50,
+            200,
+            1,
+            100
         )
 
-        this.settings.push([this.iterations, this.simulations, this.selectedColor])
+        this.coolingHeuristic = AlgorithmSettingObject.newOptionSetting(
+            "Temperature Cooling Heuristic",
+            ["Linear", "Exponential"],
+            "Exponential"
+        )
+
+
+
+        this.temperatureGradient = AlgorithmSettingObject.newOptionSetting(
+            "Temperature Color Gradient",
+            ["Polar", "Linear"],
+            "Linear");
+
+        this.gradientSegments = AlgorithmSettingObject.newRangeSetting(
+            "Color Gradient Segments",
+            5,
+            30,
+            1,
+            10);
+
+        this.temperatureStartColor = AlgorithmSettingObject.newColorSetting(
+            "Temperature Hot Color",
+            "#ff0000"
+        );
+
+        this.temperatureEndColor = AlgorithmSettingObject.newColorSetting(
+            "Temperature Cold Color",
+            "#0000ff"
+        )
+
+        this.settings.push([
+            this.iterations,
+            this.simulations,
+            this.coolingHeuristic,
+            this.initialTemperature,
+            this.temperatureGradient,
+            this.gradientSegments,
+            this.temperatureStartColor,
+            this.temperatureEndColor]);
+
         this.setRequiredProperty("Cycle")
+        this.ACCEPTANCE = 0.0001;
     }
 
     /**
@@ -45,28 +90,38 @@ class Opt3 extends AbstractTSPAlgorithm{
      * @returns {[]}
      */
     async getAnimations(vertices, edges, is3D){
+        let temperature = this.initialTemperature.obj.value;
+        const colorGradient = this.temperatureGradient.obj.value === "Linear"?
+            new LinearColorGradient(
+                this.temperatureStartColor.obj.value,
+                this.temperatureEndColor.obj.value,
+                this.gradientSegments.obj.value):
+            new PolarColorGradient(
+                this.temperatureStartColor.obj.value,
+                this.temperatureEndColor.obj.value,
+                this.gradientSegments.obj.value);
+
+        //set up intial cycle definitions & first animation
+
         //set up animations
         const animations = [];
         const firstFrameAnimation = [];
         //set up intial cycle definitions & first animation
         let path = [];
-        const initialColors = [];
-        const initialAlpha = [];
+
         let root = edges[0].start;
-        initialColors.push(edges[0].color)
-        initialAlpha.push(edges[0].alpha)
+
         firstFrameAnimation.push(edges[0].copyEdge())
         path.push(root)
 
         for(let i = 1; i < edges.length; i++){
             path.push(edges[i].start);
-            initialColors.push(edges[i].color);
-            initialAlpha.push(edges[i].alpha);
             firstFrameAnimation.push(edges[i].copyEdge())
         }
+
         path.push(root)
         animations.push(firstFrameAnimation);
-
+        let currentEdges = firstFrameAnimation;
         for(let j = 0; j < this.iterations.obj.value; j++){
             //try and perform a swap that improves the below distance
             let newPath = [];
@@ -79,7 +134,7 @@ class Opt3 extends AbstractTSPAlgorithm{
                 c = Math.floor(Math.random()*(path.length-3))+2;
 
                 [a,b,c] = [a,b,c].sort(function(a,b) {return a - b});
-                const values = this.reverseSegmentIfBetter(path, a, b, c, vertices, is3D);
+                const values = this.reverseSegmentIfBetter(path, a, b, c, vertices, is3D, temperature);
                 newPath = values[0];
                 betterSolution = values[1];
                 if(betterSolution) {
@@ -91,29 +146,22 @@ class Opt3 extends AbstractTSPAlgorithm{
             if(betterSolution){
                 const newEdges = [];
                 for(let i = 0; i < path.length - 1; i++){
-                    const e = edges[i].copyEdge();
+                    const e = currentEdges[i].copyEdge();
                     e.start = newPath[i];
                     e.end = newPath[i+1];
                     newEdges.push(e);
                     if(i ===  a || i === b || i === c){
-                        newEdges[i].color = this.selectedColor.obj.value
+                        let degree = (this.initialTemperature.obj.value - temperature)/(this.initialTemperature.obj.value)
+                        degree = Math.floor(degree * this.gradientSegments.obj.value)
+                        newEdges[i].color = colorGradient.getColorGradientColor({degree: degree}, 0)
                         newEdges[i].alpha = 0.5
-                    } else{
-                        newEdges[i].color = initialColors[i]
-                        newEdges[i].alpha = initialAlpha[i];
                     }
                 }
                 animations.push(newEdges)
+                currentEdges = newEdges
+                temperature = this.cool(temperature, j)
             }
         }
-        //add one last animation where all the edges have their intial colors;
-        const lastAnimationFrame = [];
-        for(let i = 0; i < animations[animations.length-1].length; i++){
-            lastAnimationFrame.push(animations[animations.length-1][i].copyEdge());
-            lastAnimationFrame[i].color = initialColors[i];
-            lastAnimationFrame[i].alpha = initialAlpha[i];
-        }
-        animations.push(lastAnimationFrame);
 
         return animations;
     }
@@ -128,7 +176,7 @@ class Opt3 extends AbstractTSPAlgorithm{
      * @param is3D is the network data three dimensional?
      * @returns {(*|boolean)[]|(*[]|boolean)[]} newPath, is better solution?
      */
-    reverseSegmentIfBetter(path, i, j, k, v, is3D){
+    reverseSegmentIfBetter(path, i, j, k, v, is3D, temperature){
         let newPath = [];
         const [A, B, C, D, E, F] = [path[i-1], path[i], path[j-1], path[j], path[k-1], path[k]]
         const d0 = distance(v[A], v[B], is3D) + distance(v[C], v[D], is3D) + distance(v[E], v[F], is3D);
@@ -136,17 +184,17 @@ class Opt3 extends AbstractTSPAlgorithm{
         const d2 = distance(v[A], v[B], is3D) + distance(v[C], v[E], is3D) + distance(v[D], v[F], is3D);
         const d3 = distance(v[A], v[D], is3D) + distance(v[E], v[B], is3D) + distance(v[C], v[F], is3D);
         const d4 = distance(v[F], v[B], is3D) + distance(v[C], v[D], is3D) + distance(v[E], v[A], is3D);
-
-        if(d0 > d1){
+        const accepted = Math.random()
+        if(d0 > d1 || accepted < temperature* this.ACCEPTANCE){
             newPath = this.reversedPath(path, i - 1, j -1);
             return [newPath, true];
-        } else if (d0 > d2){
+        } else if (d0 > d2 || accepted < temperature* this.ACCEPTANCE){
             newPath = this.reversedPath(path, j - 1, k - 1);
             return [newPath, true];
-        } else if(d0 > d4){
+        } else if(d0 > d4 || accepted < temperature* this.ACCEPTANCE){
             newPath = this.reversedPath(path, i - 1, k - 1);
             return [newPath, true];
-        } else if(d0 > d3){
+        } else if(d0 > d3 || accepted < temperature* this.ACCEPTANCE){
             newPath = this.reversedPathThree(path, i, j, k);
             return [newPath, true];
         }
@@ -207,12 +255,33 @@ class Opt3 extends AbstractTSPAlgorithm{
         return newPath;
     }
 
+    /**
+     * Applies the cooling heuristic to the temperature based on the user selected cooling heuristic
+     * @param temperature
+     * @param i the iteration of the algorithm we are on
+     * @returns {*}
+     */
+    cool(temperature,  i){
+        if(this.coolingHeuristic.obj.value === "Linear"){
+            return this.initialTemperature.obj.value * (this.iterations.obj.value - i)/(this.iterations.obj.value)
+        } else if(this.coolingHeuristic.obj.value === "Exponential"){
+            return 0.996 * temperature;
+        }
+        return temperature
+    }
+
     calculateDistancePath(path, vertices, is3D){
         var totalDist = 0;
         for(let i = 0; i < path.length -1 ; i++){
             totalDist += distance(vertices[path[i]], vertices[path[i+1]], is3D)
         }
         return Math.sqrt(totalDist)
+    }
+
+    copyAllEdges(edges){
+        const res = [];
+        edges.forEach((e) => res.push(e.copyEdge()))
+        return res;
     }
 }
 
@@ -224,4 +293,4 @@ function distance(v1, v2, is3D){
     return dist
 }
 
-export default Opt3;
+export default Opt3Annealing;
